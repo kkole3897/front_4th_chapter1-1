e6e2ff41c153d76e39fc23ea6e163c5abd5dfbf1
Jin Kwan Kim <kkole3897@gmail.com>
2024-12-15 22:23:21 +0900

    feat: Users model validateUser 추가

--- src/entities/user/model/users.js
+++ src/entities/user/model/users.js
@@ -1,5 +1,12 @@
 const USERS_KEY = "users";
 
+class InvalidUserError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "InvalidUserError";
+  }
+}
+
 export class Users {
   constructor() {
     const rawUsers = localStorage.getItem(USERS_KEY);
@@ -20,6 +27,12 @@ export class Users {
   }
 
   addUser(user) {
+    if (this.validateUser(user)) {
+      throw InvalidUserError(
+        `Invalid id(${user.id}) or password(${user.password})`,
+      );
+    }
+
     this.users.push(user);
 
     localStorage.setItem(USERS_KEY, JSON.stringify(this.users));
